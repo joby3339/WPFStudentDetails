69b469df73c83223bfb863ad50b083e8aa5bb97f
Joby <joby3339@gmail.com>
2025-12-03 23:02:39 +0530

    Initial commit: WPF Student Details application

---
+++ WPFStudentDetails/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace WPFStudentDetails.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }        
+        public string? Name { get; set; }
+        public int Age { get; set; }
+        public string? Grade { get; set; }
+
+    }
+}
---
+++ WPFStudentDetails/ViewModels/StudentViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Text;
+using WPFStudentDetails.Models;
+
+namespace WPFStudentDetails.ViewModels
+{
+    public class StudentViewModel
+    {
+        public ObservableCollection<Student> Students { get; set; } = new ObservableCollection<Student>();
+
+        public StudentViewModel()
+        {
+            LoadStudents();
+        }
+
+        private void LoadStudents()
+        {
+            var baseDir = AppDomain.CurrentDomain.BaseDirectory;
+            var jsonPath = Path.Combine(baseDir, "Students.json");
+
+            if (!File.Exists(jsonPath))
+            {
+                // Fallback: try project root during dev/run configurations
+                var altPath = Path.Combine(Directory.GetCurrentDirectory(), "Students.json");
+                jsonPath = File.Exists(altPath) ? altPath : jsonPath;
+            }
+
+            if (File.Exists(jsonPath))
+            {
+                var jsonData = File.ReadAllText(jsonPath);
+                var studentList = JsonConvert.DeserializeObject<List<Student>>(jsonData) ?? new List<Student>();
+                Students = new ObservableCollection<Student>(studentList);
+            }
+            else
+            {
+                Students = new ObservableCollection<Student>();
+            }
+        }
+    }
+}
